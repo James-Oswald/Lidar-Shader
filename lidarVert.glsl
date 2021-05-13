b302378234b5f84ceae902d5a8e1aa8faded57be
James-Oswald <jamesoswald111@gmail.com>
2021-05-13 05:59:32 -0400

    update settings

--- lidarVert.glsl
+++ lidarVert.glsl
@@ -7,6 +7,6 @@ varying vec3 vertexColor;
 
 void main() {
     vec4 worldPostion = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
-    vertexColor = vec3(0, 0, distance(cameraPosition, worldPostion.xyz) / 100.0);
+    vertexColor = vec3(0, 0, distance(cameraPosition, worldPostion.xyz) / 70.0);
     gl_Position = worldPostion;
 }
