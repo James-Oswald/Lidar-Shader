e188004879bf0c4c4d42c7752a487417e7b95464
James-Oswald <jamesoswald111@gmail.com>
2021-05-13 05:53:31 -0400

    added naive lidar shader

--- index.js
+++ index.js
@@ -1,49 +1,30 @@
 
-
-let camera, controls, house;
-const scene = new THREE.Scene();
-const renderer = new THREE.WebGLRenderer({antialias: false});
-const loader = new THREE.OBJLoader();
-const clock = new THREE.Clock();
-
-
-function error(msg)
-{
-	console.log(msg);
-}
-
-function progress(xhr)
-{
-	console.log((xhr.loaded / xhr.total * 100) + "% loaded");
-}
-
-function init(vertexShader, fragmentShader){
-    let width  = window.innerWidth;
-    let height = window.innerHeight;
+//Code for the camera scene
+function initCamera(){
+    let canvas = document.getElementById("camera");
+    let camera, house;
+    const scene = new THREE.Scene();
+    const renderer = new THREE.WebGLRenderer({antialias: false, canvas:canvas});
+    const loader = new THREE.OBJLoader();
+    const clock = new THREE.Clock();
+    let width  = canvas.clientWidth;
+    let height = canvas.clientHeight;
     renderer.setSize(width, height);
     renderer.setClearColor(0xffffff, 1);
-
+    camera = new THREE.PerspectiveCamera(45, width / height, 1, 1000);
+    //MTLLoader is dumb and forces us to illuminate with ambient lighting, refuses to read illum in the .mtl file
     sunlight = new THREE.AmbientLight(0xffffff);
 	scene.add(sunlight);
-    
-    camera = new THREE.PerspectiveCamera(45, width / height, 1, 1000);
-    document.body.appendChild(renderer.domElement);
 
-    /*
-    controls = new THREE.FlyControls(camera, renderer.domElement);
-    controls.movementSpeed = 10;
-	controls.domElement = renderer.domElement;
-	controls.rollSpeed = Math.PI / 5
-	controls.autoForward = false;
-	controls.dragToLook = false;
-    */
-
+    //Load the object
+    let error = function(msg){console.log(msg);}
+    let progress = function(xhr){console.log((xhr.loaded / xhr.total * 100) + "% loaded");}
     let mtlLoader = new THREE.MTLLoader();
     mtlLoader.setTexturePath("res/");
 	mtlLoader.load("res/farmhouse.mtl", function(materials) 
 	{
 		materials.preload();
-		var objLoader = new THREE.OBJLoader();
+		let objLoader = new THREE.OBJLoader();
 		objLoader.setMaterials(materials);
 		//objLoader.setPath("res/");
 		objLoader.load("res/farmhouse.obj", function(object) 
@@ -55,32 +36,88 @@ function init(vertexShader, fragmentShader){
 		}, progress, error);
 	}, progress, error);
     animate();
+
+    function animate(){
+        requestAnimationFrame(animate);
+        const delta = clock.getDelta();
+        if(house)
+            house.rotation.y += delta
+        //controls.update(delta);
+        renderer.render(scene, camera);
+    }
+
+    //Handle resizing
+    window.addEventListener( 'resize', onWindowResize, false );
+    function onWindowResize(){
+        camera.aspect = canvas.clientWidth/canvas.clientHeight;
+        camera.updateProjectionMatrix();
+        renderer.setSize(canvas.clientWidth, canvas.clientHeight);
+    }
 }
 
-function animate(){
-	requestAnimationFrame(animate);
-    const delta = clock.getDelta();
-    if(house)
-        house.rotation.y += delta
-    //controls.update(delta);
-	renderer.render(scene, camera);
+
+
+//Code for the camera scene
+function initLidar(){
+    let canvas = document.getElementById("lidar");
+    let camera, house;
+    const scene = new THREE.Scene();
+    const renderer = new THREE.WebGLRenderer({antialias: false, canvas:canvas});
+    const loader = new THREE.OBJLoader();
+    const clock = new THREE.Clock();
+    let width  = canvas.clientWidth;
+    let height = canvas.clientHeight;
+    renderer.setSize(width, height);
+    renderer.setClearColor(0xffffff, 1);
+    camera = new THREE.PerspectiveCamera(45, width / height, 1, 1000);
+
+    //Load the object
+    let error = function(msg){console.log(msg);}
+    let progress = function(xhr){console.log((xhr.loaded / xhr.total * 100) + "% loaded");}
+    loader.load("res/farmhouse.obj", function(object) 
+    {
+        house = object;
+        house.position.y = -10;
+        house.position.z = -60;
+        //scene.add(house);
+        //Load in our shaders
+        Promise.all([
+            fetch("lidarVert.glsl"),
+            fetch("lidarFrag.glsl")
+        ])
+        .then(res => Promise.all(res.map(r=>r.text())))
+        .then(data =>{
+            const material = new THREE.ShaderMaterial({vertexShader:data[0],fragmentShader:data[1]});
+            //Three.js makes copying materials to objects loaded via OBJLoader painful, see
+            //https://stackoverflow.com/questions/16200082/assigning-materials-to-an-objloader-model-in-three-js
+            house.traverse(function(child){
+                if (child instanceof THREE.Mesh)
+                    child.material = material;
+            });
+            console.log(house);
+            scene.add(house);
+        })
+        .catch(err => console.log(err));
+        
+    }, progress, error);
+    animate();
+
+    function animate(){
+        requestAnimationFrame(animate);
+        const delta = clock.getDelta();
+        if(house)
+            house.rotation.y += delta
+        renderer.render(scene, camera);
+    }
+
+    //Handle resizing
+    window.addEventListener('resize', onWindowResize, false );
+    function onWindowResize(){
+        camera.aspect = canvas.clientWidth/canvas.clientHeight;
+        camera.updateProjectionMatrix();
+        renderer.setSize(canvas.clientWidth, canvas.clientHeight);
+    }
 }
 
-//Handle resizing
-window.addEventListener( 'resize', onWindowResize, false );
-function onWindowResize(){
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-}
-
-Promise.all([
-    fetch("res/vert.glsl"),
-    fetch("res/frag.glsl")
-]).then(res =>
-    Promise.all(res.map(r=>r.text()))
-).then(data =>
-    init(...data)
-).catch(err =>
-    console.log(err)
-);
+initCamera();
+initLidar();
